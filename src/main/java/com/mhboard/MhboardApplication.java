26a4cd87145c2494ebbc6fec9b31fab169ed1760
MHLEE12 <coolmh12@gmail.com>
2022-10-30 23:28:00 +0900

    Feat: 게시판 프로젝트 생성
    게시판 프로젝트 생성

---
+++ src/main/java/com/mhboard/MhboardApplication.java
@@ -0,0 +1,13 @@
+package com.mhboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MhboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mhboard/MhboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

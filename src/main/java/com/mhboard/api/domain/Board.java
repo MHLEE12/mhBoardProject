d7b497d68d20cd676fcb458f1a9aa4e9150552b1
MHLEE12 <coolmh12@gmail.com>
2022-11-12 22:59:23 +0900

    Test: Board write 테스트 코드 작성
    게시글 작성 테스트 코드 작성

--- src/main/java/com/mhboard/api/domain/Board.java
+++ src/main/java/com/mhboard/api/domain/Board.java
@@ -1,12 +1,13 @@
 package com.mhboard.api.domain;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class Board {
 
--- src/test/java/com/mhboard/api/controller/BoardControllerTest.java
+++ src/test/java/com/mhboard/api/controller/BoardControllerTest.java
@@ -1,21 +1,34 @@
 package com.mhboard.api.controller;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.mhboard.api.domain.Board;
+import com.mhboard.api.repository.BoardRepository;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@AutoConfigureMockMvc
+@SpringBootTest
 class BoardControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @BeforeEach
+    void clean() {
+        boardRepository.deleteAll();
+    }
 
     @Test
     @DisplayName("/board 요청시 Hello World를 출력한다!")
@@ -45,6 +58,26 @@ class BoardControllerTest {
                 .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
                 .andExpect(jsonPath("$.validation.title").value("제목을 입력해주세요."))
                 .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("/board 요청시 DB에 값이 저장된다.")
+    void writeTest() throws Exception {
+
+        // when
+        mockMvc.perform(post("/board")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다.\"}")
+                )
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+
+        // then
+        assertEquals(1L, boardRepository.count());
+
+        Board board = boardRepository.findAll().get(0);
+        assertEquals("제목입니다.", board.getTitle());
+        assertEquals("내용입니다.", board.getContent());
     }
 
 }

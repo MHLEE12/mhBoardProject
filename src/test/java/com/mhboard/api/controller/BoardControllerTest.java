851c854c481659d9706ceb8ae4e299fb1f7bf99f
MHLEE12 <coolmh12@gmail.com>
2022-11-14 22:39:56 +0900

    Test: Builder로 변환 후 테스트
    Builder 추가 후 테스트

--- src/test/java/com/mhboard/api/controller/BoardControllerTest.java
+++ src/test/java/com/mhboard/api/controller/BoardControllerTest.java
@@ -1,11 +1,12 @@
 package com.mhboard.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mhboard.api.domain.Board;
 import com.mhboard.api.repository.BoardRepository;
+import com.mhboard.api.request.BoardWrite;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,12 +16,15 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@AutoConfigureMockMvc
 @SpringBootTest
+@AutoConfigureMockMvc
 class BoardControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Autowired
     private BoardRepository boardRepository;
@@ -32,26 +36,42 @@ class BoardControllerTest {
 
     @Test
     @DisplayName("/board 요청시 Hello World를 출력한다!")
-    void test() throws Exception {
+    void board_test() throws Exception {
+        // given
+//        BoardWrite request = new BoardWrite("제목입니다.", "내용입니다.");
+        BoardWrite request = BoardWrite.builder()
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .build();
+
+        // ObjectMapper는 실무에서 많이 사용함
+        String json = objectMapper.writeValueAsString(request);
+//        System.out.println("json = " + json);
 
         // expected
         mockMvc.perform(post("/board")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다.\"}")
+                        .content(json)
                 )
                 .andExpect(status().isOk())
-                .andExpect(content().string("{}"))
+                .andExpect(content().string(""))
                 .andDo(MockMvcResultHandlers.print());
     }
 
     @Test
     @DisplayName("/board 요청시 title값은 필수다.")
-    void test2() throws Exception {
+    void title_test() throws Exception {
+        // given
+        BoardWrite request = BoardWrite.builder()
+                .content("내용입니다.")
+                .build();
+
+        String json = objectMapper.writeValueAsString(request);
 
         // expected
         mockMvc.perform(post("/board")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\": null, \"content\": \"내용입니다.\"}")
+                        .content(json)
                 )
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.code").value("400"))
@@ -63,11 +83,18 @@ class BoardControllerTest {
     @Test
     @DisplayName("/board 요청시 DB에 값이 저장된다.")
     void writeTest() throws Exception {
+        // given
+        BoardWrite request = BoardWrite.builder()
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .build();
+
+        String json = objectMapper.writeValueAsString(request);
 
         // when
         mockMvc.perform(post("/board")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다.\"}")
+                        .content(json)
                 )
                 .andExpect(status().isOk())
                 .andDo(MockMvcResultHandlers.print());

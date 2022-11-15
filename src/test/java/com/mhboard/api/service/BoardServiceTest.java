00fade874b92556b18bb4cbf68a3a1286fbb6566
MHLEE12 <coolmh12@gmail.com>
2022-11-15 23:11:33 +0900

    Test: BoardService 테스트 추가
    글 작성에 대한 BoardService 테스트 추가

---
+++ src/test/java/com/mhboard/api/service/BoardServiceTest.java
@@ -0,0 +1,41 @@
+package com.mhboard.api.service;
+
+import com.mhboard.api.domain.Board;
+import com.mhboard.api.repository.BoardRepository;
+import com.mhboard.api.request.BoardWrite;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BoardServiceTest {
+
+    @Autowired
+    private BoardService boardService;
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    @DisplayName("글 작성")
+    void test1() {
+        // given
+        BoardWrite boardWrite = BoardWrite.builder()
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .build();
+
+        // when
+        boardService.write(boardWrite);
+
+        // then
+        assertEquals(1L, boardRepository.count());
+        Board board = boardRepository.findAll().get(0);
+        assertEquals("제목입니다.", board.getTitle());
+        assertEquals("내용입니다.", board.getContent());
+    }
+}

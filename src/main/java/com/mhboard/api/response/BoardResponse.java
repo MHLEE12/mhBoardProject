5e89366cc74dc4a5513ecc62c60aa9e108bcfd49
MHLEE12 <coolmh12@gmail.com>
2022-11-16 22:46:18 +0900

    Feat: 서비스 정책 적용할 response class 추가
    서비스 정책에 맞는 응답 클래스 분리하기

---
+++ src/main/java/com/mhboard/api/response/BoardResponse.java
@@ -0,0 +1,28 @@
+package com.mhboard.api.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * 서비스 정책에 맞는 응답 클래스 분리하기
+ */
+//@Builder
+@Getter
+public class BoardResponse {
+
+    private final Long no;
+    private final String title;
+    private final String content;
+
+    // if 클라이언트 요청) 제목 10글자만 표현할 수 있게 함
+//    public String getTitle() {
+//        return this.title.substring(0, 10);
+//    }
+
+    @Builder
+    public BoardResponse(Long no, String title, String content) {
+        this.no = no;
+        this.title = title.substring(0, Math.min(title.length(), 10));
+        this.content = content;
+    }
+}

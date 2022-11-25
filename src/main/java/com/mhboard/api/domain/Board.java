f1459c3cb54881327ca50fe4692552d6d96717e2
MHLEE12 <coolmh12@gmail.com>
2022-11-25 23:08:37 +0900

    Comment: 주석 삭제

--- src/main/java/com/mhboard/api/domain/Board.java
+++ src/main/java/com/mhboard/api/domain/Board.java
@@ -1,9 +1,6 @@
 package com.mhboard.api.domain;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -27,10 +24,5 @@ public class Board {
         this.content = content;
     }
 
-    // 제목 10글자로 제한
-//    public String getTitle() {
-//        // 엔티티에 getter 메소드를 만들 때에는
-//        // 서비스의 정책을 절대 넣지 마세요!
-//        return this.title.substring(0, 10);
-//    }
+
 }

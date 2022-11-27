5e39cac107c6f8f0b75816d6c82307160cbf906d
MHLEE12 <coolmh12@gmail.com>
2022-11-27 23:03:38 +0900

    Test: 게시글 수정 리펙토링 테스트

--- src/test/java/com/mhboard/api/service/BoardServiceTest.java
+++ src/test/java/com/mhboard/api/service/BoardServiceTest.java
@@ -148,4 +148,30 @@ class BoardServiceTest {
         assertEquals("내용 수정 테스트", changeBoard.getContent());
     }
 
+    @Test
+    @DisplayName("글 내용 수정")
+    void test6() {
+        // given
+        Board board = Board.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+        boardRepository.save(board);
+
+        BoardEdit boardEdit = BoardEdit.builder()
+                .title(null)
+                .content("내용 수정 테스트")
+                .build();
+
+        // when
+        boardService.edit(board.getNo(), boardEdit);
+
+        //then
+        Board changeBoard = boardRepository.findById(board.getNo())
+                .orElseThrow(() -> new RuntimeException("글이 존재하지 않습니다. no= " + board.getNo()));
+
+        assertEquals("제목", changeBoard.getTitle());
+        assertEquals("내용 수정 테스트", changeBoard.getContent());
+    }
+
 }

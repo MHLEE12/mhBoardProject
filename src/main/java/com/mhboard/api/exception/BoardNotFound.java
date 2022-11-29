459ae69819f4218f139406bc7636290c2a24e587
MHLEE12 <coolmh12@gmail.com>
2022-11-29 23:21:01 +0900

    Feat: BoardNotFound 예외처리 클래스 추가

---
+++ src/main/java/com/mhboard/api/exception/BoardNotFound.java
@@ -0,0 +1,11 @@
+package com.mhboard.api.exception;
+
+public class BoardNotFound extends RuntimeException {
+
+    private static final String MESSAGE = "존재하지 않는 글입니다.";
+
+    public BoardNotFound() {
+        super(MESSAGE);
+    }
+
+}
